c0dddf2f7209d643e00dcad7d3f734120a946b43
André Luiz Pardal <alpardal@gmail.com>
2015-12-01 14:02:04 -0200

    use small smiley image

--- app/view.js
+++ app/view.js
@@ -1,7 +1,7 @@
 import {Canvas} from './canvas';
 import {App} from './app';
 
-const image_path = 'images/Yosemite_Falls_small_bw.jpg';
+const image_path = 'images/smiley_small.jpg';
 const NUMBER_OF_CANVASES = 4;
 
 const proto = {
